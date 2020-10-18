c5cf3acded354eeb874e24d5c46defa06666f3b5
Kirill Tikach <70844215+revoltkir@users.noreply.github.com>
2020-10-18 18:17:52 +0300

    feat: add solutions basic-js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,61 @@
+  
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(param) {
+    this.param = param;
+  }
+  encrypt(message, key) {
+    if (!message || !key) throw new Error();
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    if (!/[A-Z]/g.test(message)) return message;
+    let messageLetterLength = message.match(/[A-Z]/g).length;
+    if (key.length < messageLetterLength) {
+      while (key.length <= messageLetterLength) key += `${key}`;
+      key = key.substring(0, messageLetterLength);
+    }
+    let shyfr = [];
+    let j = 0;
+    for (i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        shyfr[i] = String.fromCharCode(
+          ((message.charCodeAt(i) + key.charCodeAt(j)) % 26) + 65
+        );
+      } else {
+        shyfr[i] = message[i];
+        j--;
+      }
+      j++;
+    }
+    if (this.param == false) return shyfr.reverse().join("");
+    else return shyfr.join("");
+  }
+  decrypt(message, key) {
+    if (!message || !key) throw new Error();
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    if (!/[A-Z]/g.test(message)) return message;
+    let messageLetterLength = message.match(/[A-Z]/g).length;
+    if (key.length < messageLetterLength) {
+      while (key.length <= messageLetterLength) key += `${key}`;
+      key = key.substring(0, messageLetterLength);
+    }
+    let shyfr = [];
+    let j = 0;
+    for (i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        shyfr[i] = String.fromCharCode(
+          ((message.charCodeAt(i) + 26 - key.charCodeAt(j)) % 26) + 65
+        );
+      } else {
+        shyfr[i] = message[i];
+        j--;
+      }
+      j++;
+    }
+    if (this.param == false) return shyfr.reverse().join("");
+    else return shyfr.join("");
   }
 }
 
